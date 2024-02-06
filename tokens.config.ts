6796bcdeea93c1e5b18eb549efaa1b872d1b787b
Zaid Ahmed <82799576+zaid-ahmed-001@users.noreply.github.com>
2024-02-06 20:24:32 +0530

    seting up index.md

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,37 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+  elements: {
+    backdrop: {
+      background: {
+        initial: '',
+        dark: ''
+      }
+    },
+
+    surface: {
+      background: {
+        base: {
+          initial: '',
+          dark: ''
+        }
+      },
+
+      secondary: {
+        backgroundColor: {
+          initial: '',
+          dark: ''
+        }
+      }
+    }
+  },
+
+  docus: {
+    body: {
+      backgroundColor: {
+        initial: '',
+        dark: ''
+      }
+    }
+  }
 })
