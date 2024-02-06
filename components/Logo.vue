316a28203fd2b7c2493cf41ed524552791c8fb34
Zaid Ahmed <82799576+zaid-ahmed-001@users.noreply.github.com>
2024-02-07 01:19:24 +0530

    Header Done

---
+++ components/Logo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div style="display: flex; justify-content: center; align-items: center;">
+    <img height="20" width="30" src="../public/img/logo.png" alt="Logo" />
+    <h1 style="margin-left: 1rem; font-size: larger; font-weight: 600;">Aim Arena</h1> 
+  </div>
+</template>
+
+<script>
+export default {
+  // Component logic goes here
+}
+</script>
