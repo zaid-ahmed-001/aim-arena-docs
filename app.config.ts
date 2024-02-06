b1b42a8d2138ce7b6b7e97ecf90883adf2b47113
Zaid Ahmed <82799576+zaid-ahmed-001@users.noreply.github.com>
2024-02-07 01:29:01 +0530

    Footer Done

--- app.config.ts
+++ app.config.ts
@@ -3,11 +3,13 @@ export default defineAppConfig({
     title: 'Aim Arena Documentation',
     description: 'The best place to start your documentation.',
     image: '/favicon2.ico',
+
     socials: {
       github: 'https://github.com/siddharth220/aim-arena-source',
       twitter: '',
       youtube: 'https://www.youtube.com/watch?v=TWg75e6QsOk&list=PLTjPwwL9_KGsuNv7Z1-B-PpBQeH6nVIVp'
     },
+
     github: {
       dir: '.starters/default/content',
       branch: 'main',
@@ -15,21 +17,34 @@ export default defineAppConfig({
       owner: 'nuxt-themes',
       edit: true
     },
+
     aside: {
       level: 0,
       collapsed: false,
       exclude: []
     },
+
     main: {
       padded: true,
       fluid: true
     },
+
     header: {
       logo: true,
       showLinkIcon: true,
       exclude: [],
       fluid: true,
       title: 'Aim Arena Documentation'
+    },
+
+    footer: {
+      credits: {
+        icon: '',
+        text: '©2023 | Aim Arena All Rights Reserverd',
+        href: ''
+      },
+
+      fluid: true
     }
   }
 })
